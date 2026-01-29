5ffa3193980481470f39f839ba96b7e4e1867a09
James John Kelly Jr <jamesk89@gmail.com>
2026-01-29 13:52:38 -0500

    Initial commit

---
+++ scene.gd
@@ -0,0 +1,62 @@
+extends Node3D
+
+@onready var camera: Camera3D = $Camera3D
+@onready var label: Label = $MarginContainer/VBoxContainer/Label
+
+var meshes: Array[MeshInstance3D] = []
+
+func _ready() -> void:
+	find_mesh_instances()
+
+func find_mesh_instances() -> void:
+	var stack: Array[Node] = get_children(true)
+	
+	while stack.size() > 0:
+		var item: Node = stack.pop_front()
+		if item is MeshInstance3D:
+			meshes.append(item as MeshInstance3D)
+
+		stack.append_array(item.get_children())
+
+func _physics_process(_delta: float) -> void:
+	var screen_position: Vector2 = get_viewport().get_mouse_position()
+	
+	var origin: Vector3 = camera.project_ray_origin(screen_position)
+	var normal: Vector3 = camera.project_ray_normal(screen_position)
+
+	var success: bool = false
+	var intersection: Dictionary
+	var intersected_mesh: MeshInstance3D
+	
+	for mesh: MeshInstance3D in meshes:
+		intersection = mesh.intersect_ray(origin, normal)
+		if not intersection.is_empty() and intersection["success"]:
+			intersected_mesh = mesh
+			success = true
+			break
+	
+	var mat: Material = null
+
+	if success:
+		var from: Vector3 = intersection["position"]
+		var to: Vector3 = intersection["position"] + intersection["normal"]
+		mat = intersected_mesh.get_surface_override_material(intersection["surface"])
+		DebugDraw3D.draw_line(from, to, Color(1.0, 0.0, 0.0, 1.0))
+
+	label.text = ( \
+		"Mouse Position 2D: %v\n" +
+		"Ray Origin: %v\n" +
+		"Ray Normal: %v\n" +
+		"Intersection: %s\n" +
+		"Point: %v\n" +
+		"Normal: %v\n" +
+		"Material: %s" \
+		) % [
+			screen_position,
+			origin,
+			normal,
+			"Yes" if success else "No",
+			intersection["position"] if success else Vector3.ZERO,
+			intersection["normal"] if success else Vector3.ZERO,
+			mat.resource_path if mat else "None"
+		]
